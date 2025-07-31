d46b8138b331045d257322de4359b95ce40494c1
shakeelsaquib <shakeelsaquib50@gmail.com>
2025-07-31 18:02:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-window.watsonAssistantChatOptions = {
-  integrationID: "YOUR_INTEGRATION_ID", // Replace with your actual integration ID
-  region: "YOUR_REGION", // e.g. "us-south"
-  serviceInstanceID: "YOUR_INSTANCE_ID", // Replace with your instance ID
-  onLoad: function(instance) { instance.render(); }
-};
-setTimeout(function() {
-  const t = document.createElement('script');
-  t.src = "https://web-chat.global.assistant.watson.appdomain.cloud/versions/latest/WatsonAssistantChatEntry.js";
-  document.head.appendChild(t);
-});
+<script>
+  window.watsonAssistantChatOptions = {
+    integrationID: "34912a6e-680b-448f-9ede-295faadcab16", 
+    region: "au-syd", 
+    serviceInstanceID: "bb623ba1-6d86-48b7-b30a-f04f5b53bc34", 
+    onLoad: async (instance) => { await instance.render(); }
+  };
+  setTimeout(function(){
+    const t=document.createElement('script');
+    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+    document.head.appendChild(t);
+  });
+</script>
